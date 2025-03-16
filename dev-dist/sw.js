9fec633ce432f0145cc6eeac1adbe7768deabf00
Yaraslau Tsipikin <arximuk2005@mail.ru>
2025-03-16 20:04:27 +0300

    fix/add chose status from booked to others

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5dda3b9'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.sl6ss705md"
+    "revision": "0.rrgot1lj7qo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
